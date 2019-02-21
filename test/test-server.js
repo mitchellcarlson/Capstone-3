40693f62481f3349dcdbf86655aeebdf8f32f708
Richard McBride <richgametv@gmail.com>
2019-02-21 02:03:55 -0500

    files

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const faker = require('faker');
+//const faker = require('faker');
 const mongoose = require('mongoose');
 
 const should = chai.should();
