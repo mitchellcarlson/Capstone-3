65b1f1c91ba54d1ed9d58477067ef4b64536a729
Richard McBride <richgametv@gmail.com>
2019-02-25 12:52:39 -0500

    config change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/r-reflections';
-exports.TEST_DATABASE_URL = process.env.DATABASE_URL ||'mongodb://localhost/test-reflections'
+exports.TEST_DATABASE_URL = process.env.DATABASE_URL ||'mongodb://localhost/test-reflections';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '86400' 
