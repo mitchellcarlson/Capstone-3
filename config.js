a3ff66b5efe9266064b06aa62ab8685cfb54ab9d
Richard McBride <richgametv@gmail.com>
2019-02-27 20:21:13 -0500

    database change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/r-reflections';
-exports.TEST_DATABASE_URL = "mongodb://cap3:pikapika1@ds145312.mlab.com:45312/capstone-3" ||'mongodb://localhost/test-reflections';
+exports.TEST_DATABASE_URL = "mongodb://cap3t:pikapika1@ds143132.mlab.com:43132/capstone-3-test" ||'mongodb://localhost/test-reflections';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '86400' 
